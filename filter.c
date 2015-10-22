047f7526d33092f36ae26e8c5cc2bf3e004bf559
soundofu <zhuzongqian@gmail.com>
2015-10-22 16:33:16 +0800

    Update filter.c
    add comment for union type define
    add demo main() function
    modify SignalFilter(last) for more perfect.

--- filter.c
+++ filter.c
@@ -1,4 +1,8 @@
 
+/**
+ *  32 raw bits in a 32-bit variable
+ */
+
 typedef union {
   struct {
     unsigned long signal_0:1;
@@ -44,23 +48,38 @@ unsigned long signal_or_result = 0;
 unsigned long signal_xor_result = 0;
 
 /**
+ * For every respect bit in signal_and_result and signal_or_result:
+ * 
  * signal_and_result = A(n) & A(n-1) & A(n-2) & ...
  * if any A(n) != 1, signal_and_result = 0;
  * signal_or_result = A(n) | A(n-1) | A(n-2) | ...
  * if any A(n) == 1, signal_or_result = 1.
  * 
- * INDEX      C1  C2  C3  C4
- * A(n)       1   1   0   0
- * A(n-1)     1   0   1   0
- * A(n-2)     1   0   1   0
- * A(n-3)     1   0   1   0
- * A(n-4)     1   0   1   0
- * -----------------------
- * AND        1   0   0   0
- * OR         1   1   1   0
- * XOR        0   1   1   0   Noise or signal change
- * !XOR       1   0   0   1
- * !XOR & OR  1   0   0   0
+ * INDEX      C1  C2  C3  C4  C5
+ * A(n)       1   1   0   0   1
+ * A(n-1)     1   0   1   0   1
+ * A(n-2)     1   0   1   0   0
+ * A(n-3)     1   0   1   0   1
+ * A(n-4)     1   0   1   0   1
+ * ------------------------------
+ * AND        1   0   0   0   0
+ * OR         1   1   1   0   1
+ * XOR        0   1   1   0   1   1: Noise or signal change, 0: signal stable
+ * !XOR       1   0   0   1   0   0: signal stable, 1: noise or signal change
+ * !XOR & OR  1   0   0   0   0   
+ * ------------------------------
+ * last & XOR 0   1   1   0   1   if last == 1
+ * result     1   1   1   0   1   this = (last & XOR) | (!XOR & OR)
+ *                        |--------------- Signal changed
+ *                |---|-------|----------- Noise, no change
+ *            |--------------------------- Signal stable, no change
+ * ------------------------------
+ * last & XOR 0   0   0   0   0   if last == 0
+ * result     1   0   0   0   0   this = (last & XOR) | (!XOR & OR)
+ *            |--------------------------- Signal changed
+ *                |---|-------|----------- Noise, no change
+ *                        |--------------- Signal stable, no change
+ * ------------------------------
  */
 
 void SignalFilterEverytime(void) {
@@ -68,9 +87,32 @@ void SignalFilterEverytime(void) {
   signal_or_result |= signal_all_32.all;
 }
 
-unsigned long SignalFilter(void) {
+unsigned long last_result = 0;
+unsigned long this_result = 0;
+unsigned long SignalFilter(unsigned long last) {
+  unsigned long tmp_result;
   signal_xor_result = signal_and_result ^ signal_or_result;
+  tmp_result = (last & signal_xor_result) | (~signal_xor_result & signal_or_result);
   signal_and_result = 0xFFFFFFFF;
   signal_or_result = 0x00000000;
-  return (~signal_xor_result & signal_or_result);
+  return tmp_result;
+}
+
+#define FILTER_CONST  20
+void UpdateSignal(void) {
+  signal_all_32.bit.signal_0 = 1;
+  signal_all_32.bit.signal_1 = 1;
+}
+void main(void) {
+  int i=0;
+  while (1) {
+    i++;
+    UpdateSignal();
+    SignalFilterEverytime();
+    if (i >= FILTER_CONST) {
+      i = 0;
+      this_result = SignalFilter(last_result);
+      last_result = this_result;          // save for next time
+    }
+  }
 }
